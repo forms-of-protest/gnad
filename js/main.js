1be2839b1817e23a3f515461e670e7888a365c83
Justin Haight <jhaight@enviroissues.com>
2020-06-03 12:01:29 -0700

    fixed expand button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 
 $(".formlist > ul").toggle();
+$(".collapse").hide();
 $(".expand").click(function(){
-    $(".formlist > ul, .glyphicon-triangle-bottom, .glyphicon-triangle-top").toggle();
+    $(".formlist > ul").each(function(){
+            $(this).show();
+    });
+    $(this).hide();
+    $(".collapse").show();
+})
+$(".collapse").click(function(){
+    $(".formlist > ul").each(function(){
+            $(this).hide();
+    });
+    $(this).hide();
+    $(".expand").show();
 })
 $(".formlist > h3").click(function(){
     $(this).siblings("ul").toggle();
